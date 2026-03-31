ea3fea20f757e3c30cffe185aa60930f3f9dd9c7
MissedBoom <azaleee.18@gmail.com>
2026-03-31 23:49:49 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,17 @@
+import os
+import discord
+from discord.ext import commands
 
+TOKEN = os.getenv("TOKEN")
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Connected as {bot.user}")
+
+bot.run(TOKEN)
